25a7f71bd7585f4a5a879158d269a589f2892eaf
Shankar <shankarjangid550@gmail.com>
2025-12-03 16:13:43 +0530

    Work done till Experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import LogoSection from "./components/LogoSection"
+import NavBar from "./components/NavBar"
+import ExperienceSection from "./sections/ExperienceSection"
+import FeatureCards from "./sections/FeatureCards"
 import Hero from "./sections/Hero"
 import ShowcaseSection from "./sections/ShowcaseSection"
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Hero />
       <ShowcaseSection />
+      <LogoSection />
+      <FeatureCards />
+      <ExperienceSection />
     </>
   )
 }
